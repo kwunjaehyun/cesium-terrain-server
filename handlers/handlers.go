14c71b4a40b2786bbb7d008266b6b9e786015753
kwunjaehyun <shinekill3@gmail.com>
2021-03-12 16:18:56 +0900

    test

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -16,6 +16,7 @@ func AddCorsHeader(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		headers := w.Header()
 		headers.Set("Access-Control-Allow-Origin", "*")
+		headers.Set("Cache-Control", "max-age=86400")
 		next.ServeHTTP(w, r)
 	})
 }
